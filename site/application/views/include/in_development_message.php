105e7354703a58063d5c4da54512bc8ec46bba79
Ashley Davis <ashley_davis10419@hotmail.com>
2015-06-30 00:17:20 +0100

    Update indev message

--- site/application/views/include/in_development_message.php
+++ site/application/views/include/in_development_message.php
@@ -2,4 +2,6 @@
 	<p><em>Note:</em> this site is currently under development.
 	You're welcome to create an account here (it'd help us test and that'd be great!) but please be aware it <strong>might be deleted</strong> at any point.
 	When it's safe to make an account for the long-term, this message will disappear.</p>
+	<p><strong>WARNING:</strong> HTTPS might not be enabled! That means that while we store your password safely and it can't be stolen after it's reached us, 
+	we have no guarantees that it isn't being listened to when you register or login. In short, <em>do not use anything but a test password</em> while this message is up.</p>
 </div>
