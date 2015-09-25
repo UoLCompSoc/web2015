f8f10c351c584c4493786ecaa9a7ac4fbfbc007c
Ashley Davis <ashley_davis10419@hotmail.com>
2015-09-25 17:05:58 +0100

    More work on webhooks

--- site/application/controllers/Githubpush.php
+++ site/application/controllers/Githubpush.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Githubpush extends CI_Controller {
 	public function index() {
+		$res = $this->input->post(NULL, TRUE);
+		$message = "Test";
 		
+		if($res != FALSE) {
+			$message .= "Hello<br>";
+			$message .= $this->input->get_request_header('X-Hub-Signature');
+		}
+		
+		$this->load->view('ghfeed.php', array('notification_message'=>$message));
 	}
 }
---
+++ site/application/views/ghfeed.php
@@ -0,0 +1,49 @@
+<?php
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php
+/*
+ * This should be the first "require" because it contains the charset,
+ * which should come directly after the <head> tag.
+ */
+$this->load->view ( 'include/head_common.php' );
+?>
+
+<title>CompSoc :: GitHub Feed</title>
+</head>
+
+<body>
+	<?php
+	$this->load->view ( 'include/navbar.php' );
+	?>
+
+	<!-- Page Content -->
+	<div class="container">
+		<?php $this->load->view('include/sitewide_banner.php'); ?>
+		<?php $this->load->view('include/notification_message.php'); ?>
+		
+		<div class="row">
+			<div class="col-lg-9">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="page-header">
+							<h2>GitHub Feed</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<?php $this->load->view('include/social_sidebar.php'); ?>
+		</div>
+	</div>
+	
+	<?php
+	$this->load->view ( 'include/footer.php' );
+	$this->load->view ( 'include/bootstrapjs.php' );
+	?>
+</body>
+</html>
