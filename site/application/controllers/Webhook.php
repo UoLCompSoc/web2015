47f04b51d80ca96d047217778c8f8958542d2075
Ashley Davis <ashley_davis10419@hotmail.com>
2015-10-02 20:04:06 +0100

    Formatter and display

--- site/application/controllers/Webhook.php
+++ site/application/controllers/Webhook.php
@@ -2,46 +2,45 @@
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Webhook extends CI_Controller {
 
-    public function __construct() {
-        parent::__construct ();
-        $this->load->helper ( 'file' );
-    }
-
-	public function index() {
-        $this->load->view ( 'index' );
+	public function __construct() {
+		parent::__construct ();
+		$this->load->helper ( 'file' );
 	}
 
-    public function update() {
-        $client_id = $this->config->item('github_client_id');
-        $client_secret = $this->config->item('github_client_secret');
+	public function index() {
+		$this->load->view ( 'index' );
+	}
 
-        $url = "https://api.github.com/users/UoLCompSoc/repos?per_page=10&client_id=" . $client_id . "&client_secret=" . $client_secret;
+	public function update() {
+		$client_id = $this->config->item ( 'github_client_id' );
+		$client_secret = $this->config->item ( 'github_client_secret' );
 
-        $decoded = json_decode($this->_getContent($url));
+		$url = "https://api.github.com/users/UoLCompSoc/repos?per_page=10&client_id=" . $client_id . "&client_secret=" . $client_secret;
 
-        foreach($decoded as $repo){
-            $collaborators = json_decode($this->_getContent($repo->contributors_url));
-            $repo->collaborator_count = sizeof($collaborators);
-        }
+		$decoded = json_decode ( $this->_getContent ( $url ) );
 
-        $filepath = APPPATH . 'logs/repocache.json';
+		foreach ( $decoded as $repo ) {
+			$collaborators = json_decode ( $this->_getContent ( $repo->contributors_url ) );
+			$repo->collaborator_count = sizeof ( $collaborators );
+		}
 
-		if (! write_file ( $filepath, json_encode($decoded), 'w' )) {
+		$filepath = APPPATH . 'logs/repocache.json';
+
+		if (! write_file ( $filepath, json_encode ( $decoded ), 'w' )) {
 			log_message ( 'error', 'Cannot write to github cache file at ' . $filepath );
 		}
-    }
+	}
 
-    function _getContent($url){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Compsoc Website');
-        $html_content = curl_exec($ch);
-        curl_close($ch);
+	function _getContent($url) {
+		$ch = curl_init ();
+		curl_setopt ( $ch, CURLOPT_URL, $url );
+		curl_setopt ( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+		curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt ( $ch, CURLOPT_USERAGENT, 'Compsoc Website' );
+		$html_content = curl_exec ( $ch );
+		curl_close ( $ch );
 
-        return $html_content;
-    }
-
+		return $html_content;
+	}
 }
--- site/application/views/projects.php
+++ site/application/views/projects.php
@@ -25,127 +25,119 @@ $this->load->view ( 'include/head_common.php' );
 	<div class="container">
 		<?php $this->load->view('include/sitewide_banner.php'); ?>
 
-        <div class="row">
-            <div class="col-lg-9">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="page-header">
-                            <h2>CompSoc Projects</h2>
-                        </div>
-                        <p>Here we're building a project portfolio, so we can show off the cool stuff we've done. A great example is the
-                            portfolio itself: a CompSoc project with multiple team members taking part.</p>
+<div class="col-lg-9">
+			<div class="row">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="page-header">
+							<h2>CompSoc Projects</h2>
+						</div>
+						<p>Here we're building a project portfolio, so we can show off the cool stuff we've done. A great example is the
+							portfolio itself: a CompSoc project with multiple team members taking part.</p>
 
-                        <p>
-                            Check out the progress <a href="https://github.com/UoLCompSoc/web2015" target="_blank">on <i class="fa fa-github"></i>GitHub
-                            </a> and throw us a star!
-                        </p>
+						<p>
+							Check out the progress <a href="https://github.com/UoLCompSoc/web2015" target="_blank">on <i class="fa fa-github"></i>GitHub
+							</a> and throw us a star!
+						</p>
 
-                        <p>
-                            <iframe src="https://ghbtns.com/github-btn.html?user=UoLCompSoc&repo=web2015&type=star&count=true&size=large"
-                                    frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
-                        </p>
-                    </div>
-                </div>
-            </div>
+						<p>
+							<iframe src="https://ghbtns.com/github-btn.html?user=UoLCompSoc&repo=web2015&type=star&count=true&size=large"
+								frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+						</p>
+					</div>
+				</div>
+			</div>
 
-            <?php $this->load->view('include/social_sidebar.php'); ?>
-        </div>
+			<div class="row">
 
-        <div class="row">
-            <div class="col-lg-9">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="page-header">
-                            <h2>CompSoc Projects</h2>
-                        </div>
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="page-header">
+							<h2>CompSoc Projects</h2>
+						</div>
 
-                        <?php
+<?php
 
-                        function timeAgo($time_ago)
-                        {
-                            $time_ago = strtotime($time_ago);
-                            $cur_time   = time();
-                            $time_elapsed   = $cur_time - $time_ago;
-                            $seconds    = $time_elapsed ;
-                            $minutes    = round($time_elapsed / 60 );
-                            $hours      = round($time_elapsed / 3600);
-                            $days       = round($time_elapsed / 86400 );
-                            $weeks      = round($time_elapsed / 604800);
-                            $months     = round($time_elapsed / 2600640 );
-                            $years      = round($time_elapsed / 31207680 );
-                            // Seconds
-                            if($seconds <= 60){
-                                return "just now";
-                            }
-                            //Minutes
-                            else if($minutes <=60){
-                                if($minutes==1){
-                                    return "one minute ago";
-                                }
-                                else{
-                                    return "$minutes minutes ago";
-                                }
-                            }
-                            //Hours
-                            else if($hours <=24){
-                                if($hours==1){
-                                    return "an hour ago";
-                                }else{
-                                    return "$hours hrs ago";
-                                }
-                            }
-                            //Days
-                            else if($days <= 7){
-                                if($days==1){
-                                    return "yesterday";
-                                }else{
-                                    return "$days days ago";
-                                }
-                            }
-                            //Weeks
-                            else if($weeks <= 4.3){
-                                if($weeks==1){
-                                    return "a week ago";
-                                }else{
-                                    return "$weeks weeks ago";
-                                }
-                            }
-                            //Months
-                            else if($months <=12){
-                                if($months==1){
-                                    return "a month ago";
-                                }else{
-                                    return "$months months ago";
-                                }
-                            }
-                            //Years
-                            else{
-                                if($years==1){
-                                    return "one year ago";
-                                }else{
-                                    return "$years years ago";
-                                }
-                            }
-                        }
+function timeAgo($time_ago) {
+	$time_ago = strtotime ( $time_ago );
+	$cur_time = time ();
+	$time_elapsed = $cur_time - $time_ago;
+	$seconds = $time_elapsed;
+	$minutes = round ( $time_elapsed / 60 );
+	$hours = round ( $time_elapsed / 3600 );
+	$days = round ( $time_elapsed / 86400 );
+	$weeks = round ( $time_elapsed / 604800 );
+	$months = round ( $time_elapsed / 2600640 );
+	$years = round ( $time_elapsed / 31207680 );
+	// Seconds
+	if ($seconds <= 60) {
+		return "just now";
+	}  // Minutes
+else if ($minutes <= 60) {
+		if ($minutes == 1) {
+			return "one minute ago";
+		} else {
+			return "$minutes minutes ago";
+		}
+	}  // Hours
+else if ($hours <= 24) {
+		if ($hours == 1) {
+			return "an hour ago";
+		} else {
+			return "$hours hrs ago";
+		}
+	}  // Days
+else if ($days <= 7) {
+		if ($days == 1) {
+			return "yesterday";
+		} else {
+			return "$days days ago";
+		}
+	}  // Weeks
+else if ($weeks <= 4.3) {
+		if ($weeks == 1) {
+			return "a week ago";
+		} else {
+			return "$weeks weeks ago";
+		}
+	}  // Months
+else if ($months <= 12) {
+		if ($months == 1) {
+			return "a month ago";
+		} else {
+			return "$months months ago";
+		}
+	}  // Years
+else {
+		if ($years == 1) {
+			return "one year ago";
+		} else {
+			return "$years years ago";
+		}
+	}
+}
 
-                        ?>
+?>
 
-                        <?php foreach($githubFeed as $repo) { ?>
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <h2><a href="<?php echo $repo->html_url;?>"><?php echo $repo->name; ?></a> <small><?php echo $repo->collaborator_count; ?> Contributors</small></h2>
-                                    <p><?php echo $repo->description; ?></p>
-                                    <p><?php echo "Last updated " . timeAgo($repo->pushed_at); ?></p>
-                                </div>
-                            </div>
-                        <?php } ?>
+<?php foreach($githubFeed as $repo) { ?>
+<div class="row">
+							<div class="col-lg-12">
+								<h2>
+									<a href="<?php echo $repo->html_url;?>"><?php echo $repo->name; ?></a> <small><?php echo $repo->collaborator_count; ?> Contributors</small>
+								</h2>
+								<p><?php echo $repo->description; ?></p>
+								<p><?php echo "Last updated " . timeAgo($repo->pushed_at); ?></p>
+							</div>
+						</div>
+<?php } ?>
 
-                    </div>
-                </div>
-            </div>
-        </div>
+</div>
+				</div>
+			</div>
+		</div>
+		<?php $this->load->view('include/social_sidebar.php'); ?>
 	</div>
-	
+
 	<?php
 	$this->load->view ( 'include/footer.php' );
 	$this->load->view ( 'include/bootstrapjs.php' );
