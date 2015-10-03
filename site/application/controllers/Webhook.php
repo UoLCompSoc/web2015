340a567cc5705fc353afc55f3b4e0ad4069e128c
Christopher <kronus7713@gmail.com>
2015-10-03 17:02:53 +0100

    Removed sort for GitHub webhook

--- site/application/controllers/Webhook.php
+++ site/application/controllers/Webhook.php
@@ -18,11 +18,6 @@ class Webhook extends CI_Controller {
 		$url = "https://api.github.com/users/UoLCompSoc/repos?per_page=10&client_id=" . $client_id . "&client_secret=" . $client_secret;
 
 		$decoded = json_decode ( $this->_getContent ( $url ) );
-		
-		usort($decoded, function($a, $b)
-        {
-            return ($a->pushed_at < $b->pushed_at);
-        });
 
         $github_data = array();
 
