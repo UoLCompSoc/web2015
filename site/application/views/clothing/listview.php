b9a69013aa491e734ab21a19101697351c08a4c0
Ashley Davis <ashley_davis10419@hotmail.com>
2015-10-05 13:29:50 +0100

    Fix formatting + display on clothing listview

--- site/application/views/clothing/listview.php
+++ site/application/views/clothing/listview.php
@@ -26,40 +26,55 @@ $this->load->view ( 'include/head_common.php' );
 	<!-- Page Content -->
 	<div class="container">
 		<?php $this->load->view('include/sitewide_banner.php'); ?>
-		
+
 		<div class="row">
 			<div class="col-lg-9">
 				<div class="panel panel-default">
 					<div class="panel-body">
-                        <div class="page-header">
-                            <h2>Active Campaigns</h2>
-                            <p><a href="/clothing/add">Add New Campaign</a></p>
-                            <p><a href="/clothing/sizelistview">View Clothing Sizes</a></p>
-                        </div>
-                        <?php
-                        foreach($active as $campaign){ ?>
-                            <h3><a href="/clothing/listview/<?php echo $campaign->id; ?>"><?php echo $campaign->name; ?></a></h3>
-                            <h4><?php echo $campaign->description; ?></h4>
-                            <h5>Expires on <?php echo $campaign->expiry_date; ?></h5>
-                        <?php } ?>
+						<div class="page-header">
+							<h2>Active Campaigns</h2>
+							<p>
+								<a href="/clothing/add">Add New Campaign</a>
+							</p>
+							<p>
+								<a href="/clothing/sizelistview">View Clothing Sizes</a>
+							</p>
+						</div>
+						<?php
+						foreach ( $active as $campaign ) :
+							?>
+							<h3>
+							<a href="/clothing/listview/<?php echo $campaign->id; ?>"><?php echo $campaign->name; ?></a>
+						</h3>
+						<h4><?php echo $campaign->description; ?></h4>
+						<h5>Expires on <?php echo $campaign->expiry_date; ?></h5>
+						<?php endforeach; ?>
 
-                        <div class="page-header">
-                            <h2>Expired Campaigns</h2>
-                        </div>
-                        <?php
-                        foreach($expired as $campaign){ ?>
-                            <h3><a href="/clothing/listview/<?php echo $campaign->id; ?>"><?php echo $campaign->name; ?></a></h3>
-                            <h4><?php echo $campaign->description; ?></h4>
-                            <h5>Expired on <?php echo $campaign->expiry_date; ?></h5>
-                        <?php } ?>
+						<div class="page-header">
+							<h2>Expired Campaigns</h2>
+						</div>
+						<div>
+						<?php
+						if (sizeof ( $expired ) == 0) {
+							echo "<p>No expired campaigns found.</p>";
+						} else {
+							foreach ( $expired as $campaign ) {
+								?>
+							<h3>
+								<a href="/clothing/listview/<?php echo $campaign->id; ?>"><?php echo $campaign->name; ?></a>
+							</h3>
+							<h4><?php echo $campaign->description; ?></h4>
+							<h5>Expired on <?php echo $campaign->expiry_date; ?></h5>
+						<?php }} ?>
+						</div>
 					</div>
 				</div>
 			</div>
-			
+
 			<?php $this->load->view('include/social_sidebar.php'); ?>
 		</div>
 	</div>
-	
+
 	<?php
 	$this->load->view ( 'include/footer.php' );
 	$this->load->view ( 'include/bootstrapjs.php' );
