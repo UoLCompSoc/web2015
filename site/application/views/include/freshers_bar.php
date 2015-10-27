777a2687e4454a8f8017774ce405841e86893eab
Ashley Davis <ashley_davis10419@hotmail.com>
2015-10-27 12:53:45 +0000

    Remove freshers banner

--- site/application/views/include/freshers_bar.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-	<div class="panel panel-primary text-center">
-		<div class="panel-heading">HEY, FRESHERS!</div>
-		<div class="panel-body">
-			If you're just learning about CompSoc, you should go and <a href="https://www.facebook.com/groups/758979347496685/">join
-				us on Facebook!</a>
-		</div>
-	</div>
-</div>
