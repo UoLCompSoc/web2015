344b5c7e9826abb3386a421f57c7d8bd51b24469
Ashley Davis <ashley_davis10419@hotmail.com>
2016-02-06 16:58:39 +0000

    Add simple howard page

---
+++ site/application/views/howard.php
@@ -0,0 +1,50 @@
+<?php
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php
+/*
+ * This should be the first "require" because it contains the charset,
+ * which should come directly after the <head> tag.
+ */
+$this->load->view ( 'include/head_common.php' );
+?>
+
+<title>CompSoc :: Howard</title>
+</head>
+
+<body>
+	<?php
+	$this->load->view ( 'include/navbar.php' );
+	?>
+
+	<!-- Page Content -->
+	<div class="container">
+		<?php $this->load->view('include/sitewide_banner.php'); ?>
+		
+		<div class="row">
+			<div class="col-lg-9">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="page-header">
+							<h2>Howard</h2>
+						</div>
+						<p>Howard is a project by the committee of 2015/2016, mainly done for fun.</p>
+						<p>If you made it here, you probably saw our reddit scraper. Ignore it for now, it's not special!</p>
+					</div>
+				</div>
+			</div>
+			
+			<?php $this->load->view('include/social_sidebar.php'); ?>
+		</div>
+	</div>
+	
+	<?php
+	$this->load->view ( 'include/footer.php' );
+	$this->load->view ( 'include/bootstrapjs.php' );
+	?>
+</body>
+</html>
