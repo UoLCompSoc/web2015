40cdd0f9b5bf7028b213656e6a78865678a64b40
Ashley Davis <ashley_davis10419@hotmail.com>
2016-06-19 15:37:59 +0100

    Commit old change removing multi sci note

--- site/application/views/include/sitewide_banner.php
+++ site/application/views/include/sitewide_banner.php
@@ -2,9 +2,4 @@
 // use this to display a message across the whole site; should be used sparingly.
 ?>
 
-<div class="alert alert-info text-center">
-<strong>LATEST NEWS:</strong><br>
-The <a class="bannerLink" href="/barcrawl">multi science bar crawl</a> is coming up soon; it's always one of the best nights out of the year and is definitely not to be missed.<br>
-You can pre-book your t-shirt and ticket <a class="bannerLink" href="/clothing">right here!</a>
-</div>
 
